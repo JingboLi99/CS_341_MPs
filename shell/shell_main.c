1f1719c86fb92e3dd689c462f20184369eb0a1d6
Schwidola0607 <khoadp2@illinois.edu>
2023-02-05 21:31:43 -0600

    Deploying shell

---
+++ shell/shell_main.c
@@ -0,0 +1,11 @@
+/**
+ * shell
+ * CS 341 - Spring 2023
+ */
+#include "shell.h"
+
+// Seperate main file used for grading. Do not modify!
+int main(int argc, char **argv) {
+    // calls the student code
+    shell(argc, argv);
+}
