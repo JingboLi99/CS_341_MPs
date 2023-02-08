c9ca51d8534f6220185d088864ad8f600ae9900f
mayan3 <mayan3@illinois.edu>
2023-02-07 20:04:23 -0600

    Deploying mini_memcheck

---
+++ mini_memcheck/mini_memcheck.c
@@ -0,0 +1,29 @@
+/**
+ * mini_memcheck
+ * CS 341 - Spring 2023
+ */
+#include "mini_memcheck.h"
+#include <stdio.h>
+#include <string.h>
+
+void *mini_malloc(size_t request_size, const char *filename,
+                  void *instruction) {
+    // your code here
+    return NULL;
+}
+
+void *mini_calloc(size_t num_elements, size_t element_size,
+                  const char *filename, void *instruction) {
+    // your code here
+    return NULL;
+}
+
+void *mini_realloc(void *payload, size_t request_size, const char *filename,
+                   void *instruction) {
+    // your code here
+    return NULL;
+}
+
+void mini_free(void *payload) {
+    // your code here
+}
---
+++ mini_memcheck/test.c
@@ -0,0 +1,11 @@
+/**
+ * mini_memcheck
+ * CS 341 - Spring 2023
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    // Your tests here using malloc and free
+    return 0;
+}
