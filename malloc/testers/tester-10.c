257944c1a26088dfabad3dce769e9a5289a123b2
Schwidola0607 <khoadp2@illinois.edu>
2023-02-19 20:07:16 -0600

    Deploying malloc

---
+++ malloc/testers/tester-10.c
@@ -0,0 +1,48 @@
+/**
+ * malloc
+ * CS 341 - Spring 2023
+ */
+#include "tester-utils.h"
+
+#define SIZE (128 * M)
+#define ITERS 100000
+
+int main(int argc, char *argv[]) {
+    malloc(1);
+
+    int i;
+    for (i = 0; i < ITERS; i++) {
+        char *a = malloc(SIZE + i);
+        if (!a)
+            return 1;
+
+        verify_write(a, SIZE);
+
+        int *b = malloc(SIZE + i);
+        if (!b)
+            return 1;
+
+        verify_write(a, SIZE + i);
+
+        if (!verify_read(a, SIZE))
+            return 1;
+        if (!verify_read(a, SIZE + i))
+            return 1;
+
+        free(a);
+        free(b);
+
+        a = malloc(2 * (SIZE + i));
+        if (!a)
+            return 1;
+
+        verify_write(a, SIZE);
+        if (!verify_read(a, SIZE))
+            return 1;
+
+        free(a);
+    }
+
+    fprintf(stderr, "Memory was allocated, used, and freed!\n");
+    return 0;
+}
---
+++ malloc/testers/tester-11.c
@@ -0,0 +1,48 @@
+/**
+ * malloc
+ * CS 341 - Spring 2023
+ */
+#include "tester-utils.h"
+
+#define SIZE (2L * 1024L * M)
+#define ITERS 10000
+
+int main(int argc, char *argv[]) {
+    malloc(1);
+    int i;
+    for (i = 0; i < ITERS; i++) {
+        // Write to end
+
+        srand(rand_today());
+        int r = rand() % 10;
+        char *a = malloc(SIZE + r);
+
+        if (!a)
+            return 1;
+
+        verify_write(a, SIZE);
+        if (!verify_read(a, SIZE))
+            return 1;
+
+        free(a);
+
+        char *b = malloc(SIZE / 2);
+        verify_write(b, SIZE / 2);
+
+        char *c = malloc(SIZE / 4);
+        verify_write(c, SIZE / 4);
+
+        if (!b || !c)
+            return 1;
+
+        if (!verify_read(b, SIZE / 2) || !verify_read(c, SIZE / 4) ||
+            overlap(b, SIZE / 2, c, SIZE / 4))
+            return 1;
+
+        free(b);
+        free(c);
+    }
+
+    fprintf(stderr, "Memory was allocated, used, and freed!\n");
+    return 0;
+}
