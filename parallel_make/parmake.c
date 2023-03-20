2dca7541dd83c3dd5c07559c0f6f595229d613ef
hamza14102 <mhusain3@illinois.edu>
2023-03-20 01:56:19 -0500

    Deploying parallel_make

---
+++ parallel_make/parmake.c
@@ -0,0 +1,14 @@
+/**
+ * parallel_make
+ * CS 341 - Spring 2023
+ */
+
+#include "format.h"
+#include "graph.h"
+#include "parmake.h"
+#include "parser.h"
+
+int parmake(char *makefile, size_t num_threads, char **targets) {
+    // good luck!
+    return 0;
+}
