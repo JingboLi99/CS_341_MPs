212983c2ade2a762f58c6ed12974b2f44fe0301a
hamza14102 <mhusain3@illinois.edu>
2023-04-10 15:39:29 -0500

    Deploying nonstop_networking

---
+++ nonstop_networking/client.c
@@ -0,0 +1,107 @@
+/**
+ * nonstop_networking
+ * CS 341 - Spring 2023
+ */
+#include "format.h"
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "common.h"
+
+char **parse_args(int argc, char **argv);
+verb check_args(char **args);
+int main(int argc, char **argv) {
+    // Good luck!
+}
+
+/**
+ * Given commandline argc and argv, parses argv.
+ *
+ * argc argc from main()
+ * argv argv from main()
+ *
+ * Returns char* array in form of {host, port, method, remote, local, NULL}
+ * where `method` is ALL CAPS
+ */
+char **parse_args(int argc, char **argv) {
+    if (argc < 3) {
+        return NULL;
+    }
+
+    char *host = strtok(argv[1], ":");
+    char *port = strtok(NULL, ":");
+    if (port == NULL) {
+        return NULL;
+    }
+
+    char **args = calloc(1, 6 * sizeof(char *));
+    args[0] = host;
+    args[1] = port;
+    args[2] = argv[2];
+    char *temp = args[2];
+    while (*temp) {
+        *temp = toupper((unsigned char)*temp);
+        temp++;
+    }
+    if (argc > 3) {
+        args[3] = argv[3];
+    }
+    if (argc > 4) {
+        args[4] = argv[4];
+    }
+
+    return args;
+}
+
+/**
+ * Validates args to program.  If `args` are not valid, help information for the
+ * program is printed.
+ *
+ * args     arguments to parse
+ *
+ * Returns a verb which corresponds to the request method
+ */
+verb check_args(char **args) {
+    if (args == NULL) {
+        print_client_usage();
+        exit(1);
+    }
+
+    char *command = args[2];
+
+    if (strcmp(command, "LIST") == 0) {
+        return LIST;
+    }
+
+    if (strcmp(command, "GET") == 0) {
+        if (args[3] != NULL && args[4] != NULL) {
+            return GET;
+        }
+        print_client_help();
+        exit(1);
+    }
+
+    if (strcmp(command, "DELETE") == 0) {
+        if (args[3] != NULL) {
+            return DELETE;
+        }
+        print_client_help();
+        exit(1);
+    }
+
+    if (strcmp(command, "PUT") == 0) {
+        if (args[3] == NULL || args[4] == NULL) {
+            print_client_help();
+            exit(1);
+        }
+        return PUT;
+    }
+
+    // Not a valid Method
+    print_client_help();
+    exit(1);
+}
---
+++ nonstop_networking/common.c
@@ -0,0 +1,5 @@
+/**
+ * nonstop_networking
+ * CS 341 - Spring 2023
+ */
+#include "common.h"
