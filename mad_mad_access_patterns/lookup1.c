299bfb1aa780f0ff97a799851a75c684692d865b
xwei16 <xwei16@illinois.edu>
2023-04-11 15:45:16 -0500

    Deploying mad_mad_access_patterns

---
+++ mad_mad_access_patterns/lookup1.c
@@ -0,0 +1,16 @@
+/**
+ * mad_mad_access_patterns
+ * CS 341 - Spring 2023
+ */
+#include "tree.h"
+#include "utils.h"
+/*
+  Look up a few nodes in the tree and print the info they contain.
+  This version uses fseek() and fread() to access the data.
+
+  ./lookup1 <data_file> <word> [<word> ...]
+*/
+
+int main(int argc, char **argv) {
+    return 0;
+}
