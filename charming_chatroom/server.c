994780e349f865ccadc9017933b143af192a91f9
Jingbo Li <jingbo2@illinois.edu>
2023-04-12 22:21:30 -0500

    chatrooms part9

--- charming_chatroom/server.c
+++ charming_chatroom/server.c
@@ -146,17 +146,21 @@ void run_server(char *port) {
         // pthread_mutex_lock(&mutex); //need to mutex lock to access global variables clients and clientsCount
         if (clientsCount < MAX_CLIENTS){ //Check if we can accept a new connection
             // int new_clientfd = accept(serverSocket, NULL, NULL);
-            int new_clientfd = accept(serverSocket, (struct sockaddr *) &clientaddr, &clientaddrsize);
-            if (new_clientfd == -1){
-                perror("**SERVER ERROR- Accept failed: ");
-                exit(EXIT_FAILURE);
-            }
+            // int new_clientfd = accept(serverSocket, (struct sockaddr *) &clientaddr, &clientaddrsize);
+            // if (new_clientfd == -1){
+            //     perror("**SERVER ERROR- Accept failed: ");
+            //     exit(EXIT_FAILURE);
+            // }
             //Given a new connection can be made, find the first position in clients to store the new client fd
             for (int i = 0; i < MAX_CLIENTS; i++){
                 if (clients[i] == -1){ //this means the cur position is available
-                    // clients[i] = accept(serverSocket, (struct sockaddr *) &clientaddr, &clientaddrsize);
+                    clients[i] = accept(serverSocket, (struct sockaddr *) &clientaddr, &clientaddrsize);
+                    if (clients[i] == -1){
+                        perror("**SERVER ERROR- Accept failed: ");
+                        exit(EXIT_FAILURE);
+                    }
                     // printf("Waiting for connection...\n");
-                    clients[i] = new_clientfd;
+                    // clients[i] = new_clientfd;
                     // printf("Connection made: client_fd=%d\n", clients[i]);
                     pthread_create(&threads[i], NULL, process_client, (void *) (intptr_t) i);
                     clientsCount++;
