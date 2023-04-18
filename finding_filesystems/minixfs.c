c3da52cdd098645d6a727ff97343a2e9a775f56e
Jingbo Li <jingbo2@illinois.edu>
2023-04-17 21:13:49 -0500

    write

--- finding_filesystems/minixfs.c
+++ finding_filesystems/minixfs.c
@@ -130,6 +130,114 @@ inode *minixfs_create_inode_for_path(file_system *fs, const char *path) {
     return new_in;
 }
 
+int write_helper(file_system *fs, const char *path, const void *buf,
+                      size_t count, off_t *off, int actual_offset) {
+
+    inode* in = get_inode(fs, path);
+
+    // Check if there is enough space
+    // I guess we should actually incorporate the "count" in here as well
+    int blocks_needed = ceil((actual_offset + (int) count) / ((int) (16 * KILOBYTE)));
+
+    int min_blk_ct = minixfs_min_blockcount(fs, path, blocks_needed);
+    if (min_blk_ct == -1) {
+        errno = ENOSPC;
+        return -1;
+    }
+    // Use offset to determine which block we should write to
+    // For example, if off == 32800, then we skip first 2 blocks and start writing at 32nd byte of third block
+    int nblks_to_skip = actual_offset / BLK_SIZE;
+
+    data_block_number block_num_to_start_write;
+    if (nblks_to_skip < NUM_DIRECT_BLOCKS) { //block to write is a direct block
+        block_num_to_start_write = in->direct[nblks_to_skip];
+    } else { //block to write is a indirect block
+        nblks_to_skip -= NUM_DIRECT_BLOCKS;
+        data_block_number* start_of_indirect_block = (data_block_number *) &(fs->data_root)[in->indirect];
+        block_num_to_start_write = start_of_indirect_block[nblks_to_skip];
+    }
+    data_block* st_blk = &fs->data_root[block_num_to_start_write];
+
+    int remaining_offset = actual_offset % BLK_SIZE;
+    void* part_of_block = ((void*) st_blk) + remaining_offset;
+
+    int cur_blk_space = BLK_SIZE - remaining_offset;
+    int toWrite = (int) count;
+
+    int hasWritten = 0;
+    // Use this to track how many blocks we have used (i.e., if we've used block number at idx 9, and 10, and 11 of direct[] array)
+    int prev_block_number_idx = nblks_to_skip;
+    while (toWrite > 0) {
+        int cur_toWrite = min(cur_blk_space, toWrite);
+        memcpy(part_of_block, buf, cur_toWrite);
+        // Update for next iteration
+        buf += cur_toWrite;
+        cur_blk_space -= cur_toWrite;
+        toWrite -= cur_toWrite;
+        hasWritten += cur_toWrite;
+        if (toWrite > 0) {
+            // move pointer to next block; check if direct or indirect
+            data_block_number nxt_blk = 0;
+            int block_number_idx = prev_block_number_idx + 1;
+            prev_block_number_idx++;
+            if (block_number_idx < NUM_DIRECT_BLOCKS) {
+                nxt_blk = in->direct[block_number_idx];
+            } else {
+                data_block_number* ind_blk_st = (data_block_number *) &(fs->data_root)[in->indirect];
+                block_number_idx -= NUM_DIRECT_BLOCKS;
+                nxt_blk = ind_blk_st[block_number_idx];
+            }
+
+            // Now that we have nxt_blk, we actually get the ptr to that block by adding from the root
+            data_block* cont_blk = &fs->data_root[nxt_blk];
+            part_of_block = ((void*) cont_blk);
+
+            // Update stuff
+            cur_blk_space = BLK_SIZE;
+        }
+    }
+    // Update filesize
+    int ptr_diff = in->size - actual_offset;
+    int extra_bytes_written = hasWritten - ptr_diff;
+    if (extra_bytes_written > 0) {
+        in->size += extra_bytes_written;
+    }
+    // Update mtim and atim
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    in->mtim = ts;
+    in->atim = ts;
+    *off += hasWritten;
+
+    return hasWritten;
+}
+
+ssize_t minixfs_write(file_system *fs, const char *path, const void *buf,
+                      size_t count, off_t *off) {
+    // X marks the spot
+    if (count > ((NUM_DIRECT_BLOCKS + NUM_INDIRECT_BLOCKS) * 16 * KILOBYTE)) {
+        errno = ENOSPC;
+        return -1;
+    }
+
+    inode* in = get_inode(fs, path);
+    if (in == NULL) { // if we need to create a new file
+        inode* new_in = minixfs_create_inode_for_path(fs, path);
+        if (new_in == NULL) { // failed to create new file
+            return -1;
+        }
+        return write_helper(fs, path, buf, count, off, (int) *off);
+    }
+    else if (is_directory(in)) { // Write to an existing directory
+        return write_helper(fs, path, buf, count, off, (int) in->size);
+    }
+    else if (is_file(in)) { //write to an existing file
+        return write_helper(fs, path, buf, count, off, (int) *off);
+    } 
+
+    return -1;
+}
+
 ssize_t minixfs_virtual_read(file_system *fs, const char *path, void *buf,
                              size_t count, off_t *off) {
     if (!strcmp(path, "info")) {
@@ -168,12 +276,6 @@ ssize_t minixfs_virtual_read(file_system *fs, const char *path, void *buf,
     return -1;
 }
 
-ssize_t minixfs_write(file_system *fs, const char *path, const void *buf,
-                      size_t count, off_t *off) {
-    // X marks the spot
-    return -1;
-}
-
 ssize_t minixfs_read(file_system *fs, const char *path, void *buf, size_t count,
                      off_t *off) {
     const char *virtual_path = is_virtual_path(path);
@@ -186,7 +288,6 @@ ssize_t minixfs_read(file_system *fs, const char *path, void *buf, size_t count,
     inode * in = get_inode(fs, path);
     if (!in){ //if this file path does not exist
         errno = ENOENT;
-        perror("READ ERROR: path does not exist- ");
         return -1;
     }    
     // We want to read "count" bytes of data, unless count > filesize - off
