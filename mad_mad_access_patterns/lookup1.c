877408f54f9253383ef77fa5b3635218378d733c
Jingbo Li <jingbo2@illinois.edu>
2023-04-24 21:36:00 -0500

    both done

--- mad_mad_access_patterns/lookup1.c
+++ mad_mad_access_patterns/lookup1.c
@@ -4,13 +4,72 @@
  */
 #include "tree.h"
 #include "utils.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define btn BinaryTreeNode
 /*
   Look up a few nodes in the tree and print the info they contain.
   This version uses fseek() and fread() to access the data.
 
   ./lookup1 <data_file> <word> [<word> ...]
 */
-
+bool rec_find(FILE * tf, size_t offset, char * word){
+  fseek(tf, offset, SEEK_SET);
+  btn cnode; // cur node to check
+  fread(&cnode, sizeof(btn), 1, tf);
+  // get word length
+  char c;
+  int word_length = 0;
+  while ((c = fgetc(tf)) != '\0' && c != EOF) word_length++;
+  // get work
+  char nodeword[word_length+1]; nodeword[word_length] = '\0';
+  fseek(tf, offset + sizeof(btn), SEEK_SET);
+  fread(nodeword, 1, word_length, tf);
+  // printf("btn size: %zu, current node word: %s\n", sizeof(cnode), nodeword);
+  // check if current node is word:
+  int val = strcmp(word, nodeword);
+  if (val ==0){ // current node contains word
+    printFound(word, cnode.count, cnode.price);
+    return true;
+  } else if ( val < 0){ //left tree contains word
+    if (cnode.left_child > 0){ //make sure left tree exists
+      return rec_find(tf, cnode.left_child, word);
+    }
+  }else{ // right tree contains node
+    if (cnode.right_child > 0){
+      return rec_find(tf, cnode.right_child, word);
+    }
+  }
+  return false;
+}
 int main(int argc, char **argv) {
-    return 0;
+  //Parse and check prerequisites
+  if (argc < 3){ //take into account the program name as 1st arg
+    printArgumentUsage();
+    return -1;
+  }
+  char * infilename = argv[1]; //input filename
+  size_t nWords = argc-2; //number of word arguments
+  FILE * tf = fopen(infilename, "r"); //open tree file (tf)
+  if (!tf){ //check input file exists
+    openFail(infilename);
+    return -1;
+  }
+  // check first 4 bytes are BTRE
+  char firstFour[5];
+  firstFour[4] = '\0';
+  fread(firstFour, 1, 4, tf);
+  if (strcmp(firstFour, "BTRE") != 0){
+    formatFail(infilename);
+    return -1;
+  }
+  for (size_t i = 0; i < nWords; i++){
+    bool found = rec_find(tf, BINTREE_ROOT_NODE_OFFSET, argv[i+2]);
+    if (!found) printNotFound(argv[i+2]);
+  }
+  if (tf) fclose(tf);
+  return 0;
 }
--- mad_mad_access_patterns/lookup2.c
+++ mad_mad_access_patterns/lookup2.c
@@ -5,6 +5,13 @@
 #include "tree.h"
 #include "utils.h"
 
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 /*
   Look up a few nodes in the tree and print the info they contain.
   This version uses mmap to access the data.
@@ -12,6 +19,83 @@
   ./lookup2 <data_file> <word> [<word> ...]
 */
 
+/**
+ * Recursive helper function
+*/
+int search(BinaryTreeNode* curr_root, char* keyword, char* addr) {
+  if (strcmp(curr_root->word, keyword) == 0) {
+    printFound(curr_root->word, curr_root->count, curr_root->price);
+    return 1;
+  }
+
+  BinaryTreeNode* left_child = NULL;
+  BinaryTreeNode* right_child = NULL;
+  if (curr_root->left_child > 0) {
+    left_child = (BinaryTreeNode*) (addr + curr_root->left_child);
+  }
+  if (curr_root->right_child > 0) {
+    right_child = (BinaryTreeNode*) (addr + curr_root->right_child);
+  }
+
+  if (left_child != NULL && strcmp(keyword, curr_root->word) < 0) {
+    return search(left_child, keyword, addr);
+  } else if (right_child != NULL && strcmp(keyword, curr_root->word) > 0) {
+    return search(right_child, keyword, addr);
+  }
+
+  // If we get here, then we didn't find it (I think?)
+  printNotFound(keyword);
+  return 0;
+}
+
 int main(int argc, char **argv) {
+
+    if (argc < 3) {
+      printArgumentUsage();
+      exit(1);
+    }
+
+    char* file_name = argv[1];
+
+    // mmap
+    struct stat sb;
+    int fd = open(file_name, O_RDONLY);
+    if (fd == -1) {
+      openFail(file_name);
+      exit(2);
+    }
+    fstat(fd, &sb);
+
+    char* addr = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (addr == MAP_FAILED) {
+      mmapFail(file_name);
+      exit(3);
+    }
+
+    // Read first 4 bytes
+    char expected[4] = {'B', 'T', 'R', 'E'};
+    if (memcmp(addr, expected, 4) != 0) {
+      openFail(file_name);
+      exit(2);
+    }
+
+    // Get pointer to root
+    BinaryTreeNode* root = (BinaryTreeNode*) (addr + 4);
+
+    // Iterate through all provided words
+    for (int i = 2; i < argc; ++i) {
+      // Print the info of the word argv[i]
+      char* curr_word = argv[i];
+      search(root, curr_word, addr);
+    }
+
+    // munmap
+    if (munmap(addr, sb.st_size) == -1) {
+      mmapFail(file_name);
+      exit(3);
+    }
+
+    close(fd);
+
     return 0;
 }
